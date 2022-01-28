e723b128f07a936be370fd11140473367de0b3f3
WalentinaBorodaj <valentinaborodaj4@gmail.com>
2022-01-28 22:22:21 +0200

    add dark-thema

--- src/js/class-thems.js
+++ src/js/class-thems.js
@@ -1,13 +1,33 @@
-import { Language } from './class-language';
+// import { Language } from './class-language';
 
-export class Thema extends Language {
-  constructor() {
-    super();
-  }
+// export class Thema extends Language {
+//   constructor() {
+//     super();
+//   }
   //   get x() {
   //     console.log();
   //   }
   //   set x(newWord) {
   //     console.log();
   //   }
-}
+// }
+
+
+
+const bodyRef = document.querySelector('body');
+const toggleRef = document.querySelector('#theme-switch-toggle');
+const footerDarktheme = document.querySelector('.footer');
+
+toggleRef.addEventListener('change', event => {
+  if (bodyRef.classList.contains('dark-theme')) {
+    bodyRef.classList.remove('dark-theme');
+    bodyRef.classList.add('light-theme');
+    footerDarktheme.classList.remove('dark-theme');
+  } else {
+    bodyRef.classList.remove('light-theme');
+    bodyRef.classList.add('dark-theme');
+    footerDarktheme.classList.add('dark-theme');
+  }
+});
+
+export { bodyRef, toggleRef, footerDarktheme };
