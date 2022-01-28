9d6e6a65330c4b082428a9cefefe6bbc25357904
sla6ak <slabakxaker@gmail.com>
2022-01-28 21:42:16 +0200

    footer ok

--- src/js/class-film.js
+++ src/js/class-film.js
@@ -6,6 +6,7 @@ export class Film {
       // это объект ссылок к DOM элементам сверстаным статически(не динамически)
       renderBox: document.querySelector('#render'),
       inputFilm: document.querySelector('#inputname'),
+      ourTeam: document.querySelector('.js-our-team'),
     };
   }
   // проверочный метод который я запустил в самом индексе сквозь все классы чтоб убедиться что все настроено
