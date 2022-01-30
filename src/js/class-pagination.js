c1b89c2fded57c612a07a732c669a25af2904921
Dmytro <dmvs.9898@gmail.com>
2022-01-30 21:16:44 +0200

    внесение изменений для пагинации

--- src/js/class-pagination.js
+++ src/js/class-pagination.js
@@ -5,13 +5,18 @@ export class Paginations extends Thema {
   constructor() {
     super();
   }
+  paginationStart = async () => {
+    const respons = await this.fetchPopularFilms();
+    this.renderFilmsCardMarkup(respons);
+    this.buildPagination();
+  };
   // *********getTotalPages()***вернет актуальное значение******
-  buildPagination = () => {
+  buildPagination = async () => {
     const optionPagin = {
-      totalItems: 100,
+      totalItems: this.totalPages,
       itemsPerPage: 10,
       visiblePages: 5,
-      page: 1,
+      page: this.currentPage,
       centerAlign: true,
       firstItemClassName: 'tui-first-child',
       lastItemClassName: 'tui-last-child',
@@ -36,7 +41,11 @@ export class Paginations extends Thema {
     this.pagination = new Pagination(this.refs.containerPagination, optionPagin);
 
     this.pagination.on('afterMove', async evt => {
-      console.log(evt);
+      // localStorage.setItem('currentPage', evt.page);
+      this.currentPage = await evt.page;
+      console.log(this.currentPage);
+
+      this.paginationStart();
 
       window.scrollTo({
         top: 0,
@@ -45,4 +54,8 @@ export class Paginations extends Thema {
       });
     });
   };
+  // getCurrentPage = () => {
+  //   const curPage = localStorage.getItem('currentPage');
+  //   return JSON.parse(curPage);
+  // };
 }
